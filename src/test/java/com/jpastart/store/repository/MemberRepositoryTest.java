3edec789f392327fead17ef3ea11f6bc638dc9d8
zezeg2 <whdgus003@gmail.com>
2022-04-12 19:24:05 +0900

    Initial commit

---
+++ src/test/java/com/jpastart/store/repository/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.jpastart.store.repository;
+
+import com.jpastart.store.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @DisplayName("1.")
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void test_1() {
+        // given
+        // when
+        // than
+        Member member = new Member();
+        member.setName("A");
+        Long saveId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+
+}
